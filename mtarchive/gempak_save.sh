16c11d09c1efc71045b7c51e1f49df95e5a2ce6c
akrherz <akrherz@iastate.edu>
2016-07-21 13:47:00 -0500

    add text dir download

--- mtarchive/gempak_save.sh
+++ mtarchive/gempak_save.sh
@@ -1,7 +1,9 @@
 # Save metfs1 produced gempak files to mtarchive
 # called with number of days ago to run, usually 1
+# set -x
 
 ddir="/isu/las/class/meteorology/mtarchive/data/"
+date3=$(date -u --date "${1} days ago" +'%y%m%d')
 date2=$(date -u --date "${1} days ago" +'%Y%m%d')
 date1=$(date -u --date "${1} days ago" +'%Y/%m/%d')
 www="http://metfs1.agron.iastate.edu/data/"
@@ -10,6 +12,26 @@ mygetter ()
 {
 	wget -q -O $2 $www/$1	
 }
+
+mkdir -p ${ddir}${date1}/text/{Public,Severe,records,Climate,boy,mod,rad,sao,syn,upa}
+
+h=( 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 )
+
+for cc in $(seq 1 24);
+do
+mygetter text/boy/${date3}${h[$cc]}.boy ${ddir}${date1}/text/boy/${date2}${h[$cc]}_boy.txt
+mygetter text/mod/${date3}${h[$cc]}.mod ${ddir}${date1}/text/mod/${date2}${h[$cc]}_mod.txt
+mygetter text/rad/${date3}${h[$cc]}.rad ${ddir}${date1}/text/rad/${date2}${h[$cc]}_rad.txt
+mygetter text/sao/${date3}${h[$cc]}.sao ${ddir}${date1}/text/sao/${date2}${h[$cc]}_sao.txt
+mygetter text/syn/${date3}${h[$cc]}.syn ${ddir}${date1}/text/syn/${date2}${h[$cc]}_syn.txt
+mygetter text/upa/${date3}${h[$cc]}.upa ${ddir}${date1}/text/upa/${date2}${h[$cc]}_upa.txt
+
+done
+
+mygetter text/Public/Public.${date4} ${ddir}${date1}/text/Public/Public_${date4}.txt
+mygetter text/Severe/Severe.${date4} ${ddir}${date1}/text/Severe/Severe_${date4}.txt
+mygetter text/Record/Record.${date4} ${ddir}${date1}/text/records/Record_${date4}.txt
+mygetter text/Climate/Climate.${date4} ${ddir}${date1}/text/Climate/Climate_${date4}.txt
 
 mkdir -p ${ddir}/${date1}/gempak/mos
 mygetter gempak/mos/${date2}00_gmos.gem ${ddir}/${date1}/gempak/mos/${date2}00_gmos.gem
