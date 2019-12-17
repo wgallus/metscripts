ee700f14e05c0082e410660e4caf772522754604
akrherz <akrherz@iastate.edu>
2019-12-17 09:05:07 -0600

    fix path

--- scripts/get_ncrfc.sh
+++ scripts/get_ncrfc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /data/rdhm_forcings
-sftp -q -b /home/ldm/scripts/get_ncrfc.txt -oIdentityFile=/home/ldm/.ssh/sftp.crh.noaa.gov.txt iastate@sftp.crh.noaa.gov
+sftp -q -b /home/meteor_ldm/projects/metscripts/scripts/get_ncrfc.txt -oIdentityFile=/home/ldm/.ssh/sftp.crh.noaa.gov.txt iastate@sftp.crh.noaa.gov
