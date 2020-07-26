8ead874abf479fe663fbaed5e5a0824f32c7bdcc
akrherz <akrherz@iastate.edu>
2020-07-26 13:35:43 -0500

    copy MRMS for local caching

--- mrms/archive2box.py
+++ mrms/archive2box.py
@@ -1,10 +1,14 @@
-"""Bundle up our MRMS data to staging for delivery to GDrive."""
+"""Bundle up our MRMS data to staging for delivery to GDrive.
+
+Runs on iem13 at the moment.
+"""
 import sys
 import datetime
 import os
 import subprocess
 
 from pyiem.util import utc, logger
+
 LOG = logger()
 
 
@@ -14,18 +18,26 @@ def do(mydir, reporterror=True):
         if reporterror:
             LOG.info("Wanted to upload dir %s, but not found?!?", mydir)
         return
-    zipfn = "%s.zip" % (mydir, )
+    zipfn = "%s.zip" % (mydir,)
     if os.path.isfile(zipfn):
         os.unlink(zipfn)
     subprocess.call("zip -r -q %s %s" % (zipfn, mydir), shell=True)
 
+    # Send to metvm0 for caching
+    remotepath = "/export/mrms/%s/%s/%s" % (mydir[:4], mydir[4:6], mydir[6:8])
+    cmd = (
+        'rsync -a --rsync-path="mkdir -p %s && rsync" ' "%s meteor_ldm@192.168.0.200:%s"
+    ) % (remotepath, zipfn, remotepath)
+    subprocess.call(cmd, shell=True)
+
+    # Send to metl60 en-route for Google Drive
     remotepath = "/stage/MRMS/%s/%s/%s" % (mydir[:4], mydir[4:6], mydir[6:8])
     cmd = (
-        "rsync -a --remove-source-files --rsync-path=\"mkdir -p %s && rsync\" "
+        'rsync -a --remove-source-files --rsync-path="mkdir -p %s && rsync" '
         "%s meteor_ldm@metl60.agron.iastate.edu:%s"
-        ) % (remotepath, zipfn, remotepath)
+    ) % (remotepath, zipfn, remotepath)
     subprocess.call(cmd, shell=True)
-    subprocess.call("rm -rf %s" % (mydir, ), shell=True)
+    subprocess.call("rm -rf %s" % (mydir,), shell=True)
 
 
 def main(argv):
@@ -43,5 +55,5 @@ def main(argv):
     do(mydir, False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(sys.argv)
